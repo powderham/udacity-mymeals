29f5d7b4f7d98c8bc1af5cd07829811e8f10195a
Edward Powderham <ed.p@europeantravelventures.com>
2017-10-06 19:09:07 +0100

    adding thunk

--- src/index.js
+++ src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import reducer from './reducers';
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+// window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 
 const store =  createStore(
   reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  applyMiddleware(thunk)
 )
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
